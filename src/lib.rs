b18c8384f7ade5a4d64f57b061a5017d0306ccd3
Andrzej Janik <vosen@vosen.pl>
2015-01-17 17:47:12 +0100

    Update mod docs with my findings about lexer grammar

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,18 @@
 #![allow(unstable)]
 #![feature(box_syntax)]
 
+
 extern crate syntax;
 
+/*
+ * Rust's lexical grammar is not unambiguous.
+ * 'a'b can parse either as <Lifetime><Lifetime
+ * or <CharLiteral><Ident>. Libsyntax lexer panic!()s on that.
+ * I've took stats from servo and rust sources.
+ * Rate of <Lifetime><Token>?<Lifetime> to <CharLiteral><Token>?<Ident>
+ * is 378 to 485 in rust and 3 to 49 in servo.
+ * That's why we go with the second choice (and raise an error).
+ */
 pub mod lexer;
 pub mod token;
 
